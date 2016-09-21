8a11d3decff04288ed565835bff9f0654b3c13bb
Yuan Lunchuan <799652013@qq.com>
2016-09-21 17:15:11 +0800

    1, Finished project.

--- src/main/java/com/wisesscu/controller/UserController.java
+++ src/main/java/com/wisesscu/controller/UserController.java
@@ -13,15 +13,13 @@ import com.wisesscu.service.IUserService;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	@Resource
+	@Autowired
 	private IUserService userService;
 
 	@RequestMapping("/showUser")
 	public String toIndex(HttpServletRequest request,Model model){
 		int userId = Integer.parseInt(request.getParameter("id"));
-		System.out.println("-------userId: "+userId);
 		User user = this.userService.getUserById(userId);
-		System.out.println("-----user: "+user.getPassword());
 		model.addAttribute("user", user);
 		return "showUser";
 	}
--- src/main/java/com/wisesscu/service/impl/UserServiceImpl.java
+++ src/main/java/com/wisesscu/service/impl/UserServiceImpl.java
@@ -1,14 +1,15 @@
 package com.wisesscu.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wisesscu.dao.IUserDao;
 import com.wisesscu.pojo.User;
 import com.wisesscu.service.IUserService;
 
-@Service("userService")
+@Service(value="userService")
 public class UserServiceImpl implements IUserService {
-	@Resource
+	@Autowired
 	private IUserDao userDao;
 
 	public User getUserById(int userId) {
