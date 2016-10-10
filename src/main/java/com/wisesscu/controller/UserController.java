36900a0ef71520814c75c76781bf171672b3aa71
Yuan Lunchuan <799652013@qq.com>
2016-10-10 15:02:22 +0800

    1, refactor code style.

--- src/main/java/com/wisesscu/controller/UserController.java
+++ src/main/java/com/wisesscu/controller/UserController.java
@@ -22,70 +22,65 @@ import com.wisesscu.service.IUserService;
 public class UserController {
 	@Autowired
 	private IUserService userService;
-
+	
 	@RequestMapping("/new")
-	public ModelAndView newPage(ModelMap model){
+	public ModelAndView newPage(ModelMap model) {
 		return new ModelAndView("users/new");
 	}
-
+	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView create(@RequestParam("userName") String userName, 
-			@RequestParam("password") String password,
-			@RequestParam("age") int age,
-			ModelMap model){
-
+	public ModelAndView create(@RequestParam("userName") String userName, @RequestParam("password") String password,
+	    @RequestParam("age") int age, ModelMap model) {
+		
 		User user = new User();
 		user.setUserName(userName);
 		user.setPassword(password);
 		user.setAge(age);
 		this.userService.save(user);
 		model.addAttribute("userName", userName);
-
+		
 		return new ModelAndView("redirect:/users/");
 	}
-
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView index(ModelMap model){
+	public ModelAndView index(ModelMap model) {
 		ArrayList<User> users = new ArrayList<User>();
 		users = this.userService.all();
 		model.addAttribute("users", users);
 		return new ModelAndView("users/index");
 	}
-
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public ResponseEntity<List<User>> listAllUsers() {
-        List<User> users = this.userService.all();
-        if(users.isEmpty()){
-            return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<List<User>>(users, HttpStatus.OK);
-    }  
-
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ResponseEntity<List<User>> listAllUsers() {
+		List<User> users = this.userService.all();
+		if (users.isEmpty()) {
+			return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+	}
+	
 	@RequestMapping(value = "/{userId}", method = RequestMethod.GET)
-	public ModelAndView show(ModelMap model, @PathVariable String userId){
+	public ModelAndView show(ModelMap model, @PathVariable String userId) {
 		model.addAttribute("user", this.userService.getUserById(Integer.parseInt(userId)));
-
+		
 		return new ModelAndView("users/show");
 	}
-
-	@RequestMapping(value="/{userId}/edit", method=RequestMethod.GET)
-	public ModelAndView edit(ModelMap model, @PathVariable String userId){
-        model.addAttribute("user", this.userService.getUserById(Integer.parseInt(userId)));
+	
+	@RequestMapping(value = "/{userId}/edit", method = RequestMethod.GET)
+	public ModelAndView edit(ModelMap model, @PathVariable String userId) {
+		model.addAttribute("user", this.userService.getUserById(Integer.parseInt(userId)));
 		return new ModelAndView("users/edit");
 	}
-
+	
 	@RequestMapping(value = "/{userId}", method = RequestMethod.POST, params = "_method=DELETE")
-	public ModelAndView destroy(ModelMap model,
-			@PathVariable String userId){
+	public ModelAndView destroy(ModelMap model, @PathVariable String userId) {
 		this.userService.destroy(Integer.parseInt(userId));
 		return new ModelAndView("redirect:/users/");
 	}
-
-	@RequestMapping(value="/{userId}", method=RequestMethod.POST)
-	public ModelAndView update(ModelMap model,
-			@PathVariable int userId,
-			@RequestParam("userName") String userName,
-			@RequestParam("age") int age){
+	
+	@RequestMapping(value = "/{userId}", method = RequestMethod.POST)
+	public ModelAndView update(ModelMap model, @PathVariable int userId, @RequestParam("userName") String userName,
+	    @RequestParam("age") int age) {
 		User user = new User();
 		user.setAge(age);
 		user.setUserName(userName);
