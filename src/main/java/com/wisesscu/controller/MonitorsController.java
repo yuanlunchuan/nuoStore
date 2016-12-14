c7ab8c9eb15204f96375467edac0d7c8d830a443
Yuan Lunchuan <799652013@qq.com>
2016-12-14 16:25:00 +0800

    1, Add pagenate.

--- src/main/java/com/wisesscu/controller/MonitorsController.java
+++ src/main/java/com/wisesscu/controller/MonitorsController.java
@@ -18,33 +18,33 @@ import com.wisesscu.websocket.MyWebSocketHandler;
 @Controller
 @RequestMapping("/monitor")
 public class MonitorsController {
-
+	
 	@RequestMapping("/new")
-	public String newMonitorPage(HttpSession session){
-		if(null==session.getAttribute("uid")){
+	public String newMonitorPage(HttpSession session) {
+		if (null == session.getAttribute("uid")) {
 			session.setAttribute("uid", UUID.randomUUID().toString());
 		}
 		return "monitor/newPage";
 	}
-
-	@RequestMapping(value="/create", method=RequestMethod.POST)
-	public Object create(String uid, String text){
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public Object create(String uid, String text) {
 		
 		Map<String, String> result = new HashMap<String, String>();
-
+		
 		result.put("uid", uid);
 		result.put("text", text);
 
 		WebSocketSession webSocketSession = MyWebSocketHandler.userSocketSessionMap.get(uid);
 		MyWebSocketHandler.sendMessageToUser(webSocketSession, new TextMessage(text));
-
+		System.out.println("------------line 40");
 		return result;
 	}
-
+	
 	public void sendMessageToUser(WebSocketSession session, TextMessage message) throws IOException {
 		if (session != null && session.isOpen()) {
 			session.sendMessage(message);
 		}
 	}
-
+	
 }
